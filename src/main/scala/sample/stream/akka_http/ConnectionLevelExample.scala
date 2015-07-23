63fecdb95ec2e3e6101943923a1b51be7602d269
PALLAVI GOLIWALE <pallavig@thoughtworks.com>
2015-07-23 11:13:52 +0530

    connection level http request

--- src/main/scala/sample/stream/akka_http/ConnectionLevelExample.scala
+++ src/main/scala/sample/stream/akka_http/ConnectionLevelExample.scala
@@ -1,5 +1,25 @@
 package sample.stream.akka_http
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpRequest
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{ Sink, Source }
+
 object ConnectionLevelExample {
+  def main(args: Array[String]) {
+
+    implicit val actorSystem = ActorSystem("akka-connection-level")
+    implicit val materialiser = ActorMaterializer()
+    implicit val executionContextExecutor = actorSystem.dispatcher
+
+    val outgoingConnection = Http().outgoingConnection("akka.io")
+
+    val eventualResponse = Source.single(HttpRequest(uri = "/"))
+      .via(outgoingConnection)
+      .runWith(Sink.head)
+
+    eventualResponse.map({ x => println(x); actorSystem.shutdown(); 1 })
+  }
 
 }
