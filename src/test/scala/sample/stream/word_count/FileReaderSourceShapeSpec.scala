67c6159b2cfde4cdb91a2f1388e6e92d779afa9b
PALLAVI GOLIWALE <pallavig@thoughtworks.com>
2015-07-28 10:01:16 +0530

    file name changed

--- src/test/scala/sample/stream/word_count/FileReaderSourceShapeSpec.scala
+++ src/test/scala/sample/stream/word_count/FileReaderSourceShapeSpec.scala
@@ -2,14 +2,14 @@ package sample.stream.word_count
 
 import akka.stream.scaladsl.Sink
 import akka.testkit.TestProbe
-import org.scalatest.{FunSpec, MustMatchers}
+import org.scalatest.{ FunSpec, MustMatchers }
 
 class FileReaderSourceShapeSpec extends FunSpec with MustMatchers {
   describe("utf-8 file source") {
     it("should read file") {
-      import FileReaderSourceShape.{actorSystem, materialiser}
+      import FileReaderSourceShape.{ actorSystem, materialiser }
 
-      val filename = "/Users/pallavig/projects/self/akka-stream-scala/src/test/resources/test-file.txt"
+      val filename = "/Users/pallavig/projects/self/akka-stream-scala/src/test/resources/test-file"
       val source = FileReaderSourceShape.utf8FileReader(filename)
       val probe = TestProbe()
 
